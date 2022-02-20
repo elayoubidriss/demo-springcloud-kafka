8d87d13286e8d94c392331ba79cbb3426f098065
elayoubidriss <drisselayoubi4@gmail.com>
2022-02-20 20:21:16 +0100

    Initial commit

---
+++ src/test/java/ma/emsi/demospringcloudkafka/DemoSpringcloudKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.emsi.demospringcloudkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringcloudKafkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
